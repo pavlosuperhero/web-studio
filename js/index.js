2fbc916c9b8a79308e92e47485dd1ac74fdbf3fc
Pavlo <pavlo.fabuliak@gmail.com>
2017-03-09 19:19:49 +0000

    30% is done

--- js/index.js
+++ js/index.js
@@ -1,8 +1,25 @@
-$(document).ready(function() {
-   $('.toggle-nav').click(function(e) {
-        $(this).toggleClass('active');
-        $('.menu ul').toggleClass('active');
- 
-        e.preventDefault();
+$('.hamburger').on('click', function(){
+  //  if($('.menu').hasClass('open')){
+    //    $('.menu').removeClass('open');
+    //}   else{
+      //  $('.menu').addClass('open');
+    //}
+  $('.menu').toggleClass('open')
+});
+$('#we_do_conteiner_of_conteiners #content_conteiner').mouseover(function(){
+     $('#advertisement',this).css('color', '#18cfab');
+});
+$('#we_do_conteiner_of_conteiners #content_conteiner').mouseleave(function(){
+     $('#advertisement',this).css('color', '#717171');
+});
+$(function() {
+    $('.main_menu').on('click', function(event) {
+        var target = $(this.getAttribute('href'));
+        if (target.length) {
+            event.preventDefault();
+            $('html, body').stop().animate({
+                scrollTop: target.offset().top
+            }, 2000);
+        }
     });
 });
